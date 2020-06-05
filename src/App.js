c1b43426b11b32c1bf12e4ca5d33fff2e5ab8c74
bim240 <bimlendu357@gmail.com>
2020-06-05 17:11:36 +0530

    loading page added, lazy loading

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import React, { Suspense, lazy } from "react";
-// import { Row } from "react-bootstrap";
-// const OtherComponent = React.lazy(() => import("./OtherComponent"));
 
-import Header from "./component/header";
-import Home from "./component/home";
-import AboutMe from "./component/aboutMe";
-import Skills from "./component/skills";
 import "./assets/stylesheets/style.css";
-import MyWork from "./component/myWork";
-import Article from "./component/article";
-import Contact from "./component/contact";
-import Footer from "./component/footer";
-// const Header = lazy(() => import("./component/header"));
-// const Home = lazy(() => import("./component/home"));
-// const AboutMe = lazy(() => import("./component/aboutMe"));
-// const Skills = lazy(() => import("./component/skills"));
-// const MyWork = lazy(() => import("./component/myWork"));
-// const Article = lazy(() => import("./component/article"));
-// const Contact = lazy(() => import("./component/contact"));
-// const Footer = lazy(() => import("./component/footer"));
+import Loading from "./component/loading/loading";
+
+const Header = lazy(() => import("./component/header"));
+const Home = lazy(() => import("./component/home"));
+const AboutMe = lazy(() => import("./component/aboutMe"));
+const Skills = lazy(() => import("./component/skills"));
+const MyWork = lazy(() => import("./component/myWork"));
+const Article = lazy(() => import("./component/article"));
+const Contact = lazy(() => import("./component/contact"));
+const Footer = lazy(() => import("./component/footer"));
 
 function App() {
   return (
     <div>
-      {/* <Suspense fallback={<div>loading</div>}> */}
-      <Header />
-      <Home />
-      <AboutMe />
-      <Skills />
-      <MyWork />
-      <Article />
-      <Contact />
-      <Footer />
-      {/* </Suspense> */}
+      <Suspense fallback={<Loading />}>
+        <Header />
+        <Home />
+        <AboutMe />
+        <Skills />
+        <MyWork />
+        <Article />
+        <Contact />
+        <Footer />
+      </Suspense>
     </div>
   );
 }
---
+++ src/component/loading/loading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./loading.css";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div class="hexlogo">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
