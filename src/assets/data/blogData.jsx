6adaf2e1c9c5d482bab934ac811b766e08c6b41c
bim240 <bimlendu357@gmail.com>
2020-06-05 14:20:40 +0530

    replaced img with svg, changed ui

---
+++ src/assets/data/blogData.jsx
@@ -0,0 +1,110 @@
+var blogData = [
+  {
+    title: "JWT Authentication",
+    description:
+      "Before we get into action, there are few things you need to know and they are not fancy, you can google them. Even if you don't know them I will do my best to make it easy for you.",
+    link: null,
+    mediumLink: "https://medium.com/@bim240/jwt-authentication-fe47f8993461",
+    img: "jwt.svg",
+    tag: ["Node"],
+    catagory: "node",
+  },
+  {
+    title: "DOM (Document Object Model)",
+    description:
+      "The Document Object Model (DOM) connects web pages to scripts or programming languages by representing the structure of a document — such as the HTML representing a web page — in memory.",
+    link: null,
+    mediumLink:
+      "https://medium.com/@bim240/dom-document-object-model-8e12d6f47c1e",
+    img: "documents.svg",
+    tag: ["JavaScript"],
+    catagory: "js",
+  },
+  {
+    title: "Variables in js",
+    description:
+      "In general, a variable represents a concept or an item whose magnitude can be represented by a number, i.e. measured quantitatively. Variables are called variables because they vary, i.e. they can have a variety of values",
+    link: null,
+    mediumLink: "https://medium.com/@bim240/variables-in-js-bc5fcb46e640",
+    img: "variable.svg",
+    tag: ["React"],
+    catagory: "react",
+  },
+
+  {
+    title: "What, and Why of Responsive web design",
+    description:
+      "Responsive web design (RWD) creates a system for a single site to react to the size of a user’s device — with one URL and one content source. A responsive website has a fluid and flexible layout which adjusts according to screen size.",
+    link: null,
+    mediumLink:
+      "https://medium.com/@bim240/what-and-why-of-responsive-web-design-29f5d3d47ec1",
+    img: "responsive.svg",
+    tag: ["HTML & CSS"],
+    catagory: "html",
+  },
+  {
+    title: "Flexbox",
+    description:
+      "As the heading suggests we will be learning about flexbox today. We will go in-depth today. It will be better if you code as we move ahead in flexbox and actually see what each piece of code does.",
+    link: null,
+    mediumLink: "https://medium.com/@bim240/flexbox-altcampus-a9627fab44ff",
+    img: "flexbox.svg",
+    tag: ["HTML & CSS"],
+    catagory: "js",
+  },
+  {
+    title: "Advanced positioning in CSS",
+    description:
+      "In this Article you will learn how to use:position -top, bottom, left and right. All of these properties work together to give an element a specific position. When you use a position value other than the default, you can position it using the top, bottom, left and right properties.",
+    link: null,
+    mediumLink:
+      "https://medium.com/@bim240/advanced-positioning-in-css-aa397c73c4",
+    img: "position.svg",
+    tag: ["HTML & CSS"],
+    catagory: "html",
+  },
+  {
+    title: "Positioning content in html&css",
+    description:
+      "CSS helps to position your HTML element. We can put any HTML element at whatever location we like. We can specify whether we want the element positioned relative to its natural position in the page or absolute based on its parent element. ",
+    link: null,
+    mediumLink:
+      "https://medium.com/@bim240/positioning-content-in-html-css-8794c6d4c2a1",
+    img: "position1.svg",
+    tag: ["HTML & CSS"],
+    catagory: "html",
+  },
+  {
+    title: "BOX MODEL",
+    description:
+      "Before moving ahead with the box model first let’s discuss the display properties, few other properties, and their uses. By default, some elements in html are block-level elements and some are inline-level elements. Here are a few examples",
+    link: null,
+    mediumLink: "https://medium.com/@bim240/box-model-altcampus-c26668dc5eed",
+    img: "box-model.svg",
+    tag: ["HTML", "CSS"],
+    catagory: "html",
+  },
+  {
+    title: "CSS IN USE",
+    description:
+      "Let’s continue what we did in the last article. Here we will cover more of the CSS part. We will cover eight different topics today, starting from CSS reset and ending with length. By the time we finish this article, we will take another step towards a full-stack developer.",
+    link: null,
+    mediumLink:
+      "https://medium.com/@bim240/css-in-use-at-altcampus-39af963e37c",
+    img: "css.svg",
+    tag: ["HTML", "CSS"],
+    catagory: "html",
+  },
+  {
+    title: "HTML & CSS",
+    description:
+      "Recently I started learning Html & CSS. HTML is used for content writing and CSS is used for designing those content. Let’s take the human body as an example to understand it better. Now here skeleton is Html and CSS part is how muscle cover our skeleton",
+    link: null,
+    mediumLink: "https://medium.com/@bim240/html-css-713c434d0946",
+    img: "html.svg",
+    tag: ["HTML", "CSS"],
+    catagory: "html",
+  },
+];
+
+export { blogData };
--- src/component/article.js
+++ src/component/article.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Flipper, Flipped } from "react-flip-toolkit";
 import { FaLink, FaMedium } from "react-icons/fa";
 import { v4 as uuid } from "uuid";
+
+import { blogData } from "../assets/data/blogData";
 // import { IoIosArrowDropdown } from "react-icons/io";
 
 class Article extends React.Component {
@@ -9,127 +11,7 @@ class Article extends React.Component {
     super(props);
     this.state = {
       collapse: false,
-      allProjects: [
-        {
-          title: "JWT Authentication",
-          description:
-            "Before we get into action, there are few things you need to know and they are not fancy, you can google them. Even if you don't know them I will do my best to make it easy for you.",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/jwt-authentication-fe47f8993461",
-          img: "jwt.png",
-          tag: ["Node"],
-          catagory: "node",
-        },
-        {
-          title: "DOM (Document Object Model)",
-          description:
-            "The Document Object Model (DOM) connects web pages to scripts or programming languages by representing the structure of a document — such as the HTML representing a web page — in memory.",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/dom-document-object-model-8e12d6f47c1e",
-          img: "dom.png",
-          tag: ["JavaScript"],
-          catagory: "js",
-        },
-        {
-          title: "Variables in js",
-          description:
-            "In general, a variable represents a concept or an item whose magnitude can be represented by a number, i.e. measured quantitatively. Variables are called variables because they vary, i.e. they can have a variety of values",
-          link: null,
-          mediumLink: "https://medium.com/@bim240/variables-in-js-bc5fcb46e640",
-          img: "variable.png",
-          tag: ["React"],
-          catagory: "react",
-        },
-        {
-          title: "AltCampus",
-          description:
-            "This article is about my 1-month experience at AltCampus.As at AltCampus, we learn technical skills so I will first discuss the technical part and other stuff later on.",
-          link: null,
-          mediumLink: "https://medium.com/@bim240/altcampus-48208a0b92d5",
-          img: "altcampus.png",
-          tag: ["AltCampus"],
-          catagory: "react",
-        },
-
-        {
-          title: "What, and Why of Responsive web design",
-          description:
-            "Responsive web design (RWD) creates a system for a single site to react to the size of a user’s device — with one URL and one content source. A responsive website has a fluid and flexible layout which adjusts according to screen size.",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/what-and-why-of-responsive-web-design-29f5d3d47ec1",
-          img: "responsive.png",
-          tag: ["HTML & CSS"],
-          catagory: "html",
-        },
-        {
-          title: "Flexbox",
-          description:
-            "As the heading suggests we will be learning about flexbox today. We will go in-depth today. It will be better if you code as we move ahead in flexbox and actually see what each piece of code does.",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/flexbox-altcampus-a9627fab44ff",
-          img: "flexbox.jpg",
-          tag: ["HTML & CSS"],
-          catagory: "js",
-        },
-        {
-          title: "Advanced positioning in CSS",
-          description:
-            "In this Article you will learn how to use:position -top, bottom, left and right. All of these properties work together to give an element a specific position. When you use a position value other than the default, you can position it using the top, bottom, left and right properties.",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/advanced-positioning-in-css-aa397c73c4",
-          img: "position.jpeg",
-          tag: ["HTML & CSS"],
-          catagory: "html",
-        },
-        {
-          title: "Positioning content in html&css",
-          description:
-            "CSS helps to position your HTML element. We can put any HTML element at whatever location we like. We can specify whether we want the element positioned relative to its natural position in the page or absolute based on its parent element. ",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/positioning-content-in-html-css-8794c6d4c2a1",
-          img: "position1.jpeg",
-          tag: ["HTML & CSS"],
-          catagory: "html",
-        },
-        {
-          title: "BOX MODEL",
-          description:
-            "Before moving ahead with the box model first let’s discuss the display properties, few other properties, and their uses. By default, some elements in html are block-level elements and some are inline-level elements. Here are a few examples",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/box-model-altcampus-c26668dc5eed",
-          img: "box-model.png",
-          tag: ["HTML", "CSS"],
-          catagory: "html",
-        },
-        {
-          title: "CSS IN USE",
-          description:
-            "Let’s continue what we did in the last article. Here we will cover more of the CSS part. We will cover eight different topics today, starting from CSS reset and ending with length. By the time we finish this article, we will take another step towards a full-stack developer.",
-          link: null,
-          mediumLink:
-            "https://medium.com/@bim240/css-in-use-at-altcampus-39af963e37c",
-          img: "css.jpeg",
-          tag: ["HTML", "CSS"],
-          catagory: "html",
-        },
-        {
-          title: "HTML & CSS",
-          description:
-            "Recently I started learning Html & CSS. HTML is used for content writing and CSS is used for designing those content. Let’s take the human body as an example to understand it better. Now here skeleton is Html and CSS part is how muscle cover our skeleton",
-          link: null,
-          mediumLink: "https://medium.com/@bim240/html-css-713c434d0946",
-          img: "html&css.png",
-          tag: ["HTML", "CSS"],
-          catagory: "html",
-        },
-      ],
+      allProjects: blogData,
     };
   }
   handleCollapse = () => {
@@ -158,11 +40,15 @@ class Article extends React.Component {
                           key={uuid()}
                         >
                           <div className="col mb-5 mt-3">
-                            <div className="card h-100 ">
+                            <div
+                              className="card h-100 border-0 shadow"
+                              style={{ backgroundColor: "#e6e6e6" }}
+                            >
+                              {" "}
                               <div className="overlay_container">
                                 <img
                                   src={`./articles/${project.img}`}
-                                  className="card-img-top"
+                                  className="card-img-top article_svg_responsive"
                                   alt="..."
                                 />
                                 <div className="work_image_overlay">
@@ -216,7 +102,6 @@ class Article extends React.Component {
                                   </a>
                                 </p>
                               </div>
-                              <div className="card-footer"></div>
                             </div>
                           </div>
                         </Flipped>
@@ -232,11 +117,14 @@ class Article extends React.Component {
                         >
                           <Flipped flipId="coolDiv" ease="easeOutExpo">
                             <div className="col mb-5 mt-3">
-                              <div className="card h-100 ">
+                              <div
+                                className="card h-100 border-0 shadow "
+                                style={{ backgroundColor: "#e6e6e6" }}
+                              >
                                 <div className="overlay_container">
                                   <img
                                     src={`./articles/${project.img}`}
-                                    className="card-img-top"
+                                    className="card-img-top article_svg_responsive"
                                     alt="..."
                                   />
                                   <div className="work_image_overlay">
@@ -290,7 +178,6 @@ class Article extends React.Component {
                                     </a>
                                   </p>
                                 </div>
-                                <div className="card-footer"></div>
                               </div>
                             </div>
                           </Flipped>
@@ -317,7 +204,7 @@ class Article extends React.Component {
               }}
             >
               {" "}
-              {!this.state.collapse ? "Show All" : "Hide"}
+              {!this.state.collapse ? "Show All" : "Show Less"}
             </a>
           </div>
         </>{" "}
