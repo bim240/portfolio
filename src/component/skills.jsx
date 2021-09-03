65eec62e1649603471bab8d054efecbbd7527dca
Bimlendu Kumar <bim240@Bimlendus-MacBook-Pro.local>
2021-09-03 17:51:31 +0530

    update skill section

--- src/component/skills.jsx
+++ src/component/skills.jsx
@@ -1,48 +1,51 @@
-import React from "react";
+import React from 'react';
 // import { Link } from "react-router-dom";
-import { v4 as uuid } from "uuid";
-import { FaHtml5, FaNode, FaReact, FaBootstrap } from "react-icons/fa";
-import { DiMongodb, DiJavascript1 } from "react-icons/di";
+import { v4 as uuid } from 'uuid';
+import { FaHtml5, FaNode, FaReact, FaBootstrap, FaSass } from 'react-icons/fa';
+import { DiMongodb, DiJavascript1, DiReact } from 'react-icons/di';
+import { GrGraphQl } from 'react-icons/gr';
+import { AiOutlineAntDesign } from 'react-icons/ai';
 
 const Skills = () => {
   let skills = [
-    { icon: <FaHtml5 />, name: " HTML & CSS" },
-    { icon: <DiJavascript1 />, name: "JavaScript" },
-    { icon: <DiMongodb />, name: "MongoDB" },
-    { icon: <FaNode />, name: "Node/Express" },
-    { icon: <FaReact />, name: "React" },
-    { icon: <FaBootstrap />, name: "Bootstrap" },
+    { icon: <FaReact />, name: 'React' },
+    { icon: <DiReact />, name: 'React Native' },
+    { icon: <FaNode />, name: 'Node/Express' },
+    { icon: <GrGraphQl />, name: 'GraphQl' },
+    { icon: <DiMongodb />, name: 'MongoDB' },
+    { icon: <DiJavascript1 />, name: 'JavaScript' },
+    { icon: <FaHtml5 />, name: 'HTML & CSS' },
+    { icon: <FaSass />, name: 'Sass' },
+    { icon: <AiOutlineAntDesign />, name: 'Ant Design' },
+    { icon: <FaBootstrap />, name: 'Bootstrap' },
   ];
   return (
     <>
-      {" "}
+      {' '}
       {/* <Jumbotron> */}
-      <div className="container" id="skills">
+      <div className='container' id='skills'>
         {/* <Link> */}
-        <h1 className="hide"> bimlendu portfolio skills section </h1>
-        <h2 className=" font-weight-bold">Skills</h2>
+        <h1 className='hide'> bimlendu portfolio skills section </h1>
+        <h2 className=' font-weight-bold'>Skills</h2>
         {/* </Link> */}
-        <p className="font-weight-bold">
+        <p className='font-weight-bold'>
           The future belongs to those who Learn more skills and combine
           <br /> them in creative ways.
         </p>
-        <div className="row mt-5">
+        <div className='row mt-5'>
           {/* html css */}
           {skills.map((skill) => {
             return (
-              <div className="col-md-3 col-sm-6 mb-5" key={uuid()}>
+              <div className='col-md-3 col-sm-6 mb-5' key={uuid()}>
                 <div
-                  className="mx-auto"
+                  className='mx-auto'
                   style={{
-                    textAlign: "center",
-                    fontSize: "4rem",
-                  }}
-                >
+                    textAlign: 'center',
+                    fontSize: '4rem',
+                  }}>
                   {skill.icon}
                 </div>
-                <h5 className="font-weight-bold text-center mt-3">
-                  {skill.name}
-                </h5>
+                <h5 className='font-weight-bold text-center mt-3'>{skill.name}</h5>
               </div>
             );
           })}
